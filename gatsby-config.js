6a4ad1e76a43a5982350f2537f691609319ba902
TahsinProduction <khantahsinkabir@gmail.com>
2019-11-02 00:06:48 +0600

    Revert "Minor Changes"
    This reverts commit 4442038be4e1142ba7c97664b7caeaeee0241fd9.

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,8 +68,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `TahsinProduction`,
-        short_name: `TahsinProduction`,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
