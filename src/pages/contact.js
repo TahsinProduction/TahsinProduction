3b96f719bee6a718c1073aa920dcd22b899db540
TahsinProduction <khantahsinkabir@gmail.com>
2019-11-10 07:09:34 +0600

    optimazied

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ const AboutPage = () => (
   <Layout pageTitle="Contact Us">
     <SEO title="ContactUS" keywords={[`TahsinProduction`, `Tahsin Production`, `Contact Us`]} />
     <div className="col-md-3"></div>
-    <img src = "https://s29vla.storage.yandex.net/rdisk/15e6b201fe6914c8f194073329683176f95d2bee0aad89811c0d5c1b7bf13aa6/5dc2ba6e/fKqInKw3d7bLFOeFnMGnhKXFCv-zr-VQXbCeoVHXJrhsxuCtBS8B2nbplSzrvTs8dSUP44fB1dXM3txotWMvfLxRpUhiGHgvxk-agTsA9J-r8npumZHI4midPdWhecNq?uid=1130000040914342&filename=Official%20Cover%20TahsinProduction.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000040914342&fsize=16121709&hid=d63684c22b8ba84e5ae5e5992155e6dd&media_type=image&tknv=v2&etag=e3753f2a7e307d3aabc915a4f3afca57&rtoken=hzz3sEMRXFGR&force_default=yes&ycrid=na-9b92214ca12be776947ff2bc68412c38-downloader10f&ts=596ac9130f780&s=77c15ece090fa1e3f32aecf2a688c733d0251aaf47d110fb02475ff40f8e4e81&pb=U2FsdGVkX1-dBXO6rHKu5HTbFRujvjYbMScply42GBDCT7po6VyfI7atMw5p_3zOqbMXOKuuqkiQqTq1-c1YzlBZpt5OTffwEfqcjAaEKirkPIdHj7swK51MqpH6v2kq" width="100%" height="auto" margin="0 auto" alt="TahsinProduction Tahsin Production" />
+    <img src = "https://i.postimg.cc/rM1htVjr/Official-Cover-Tahsin-Production-1.jpg" width="100%" height="auto" margin="0 auto" alt="TahsinProduction Tahsin Production" />
 <center><h1>If you have any query or problem or want to say something to us then please fill the form below</h1><h1>You will receive reply shortly</h1></center>
 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSce1xHd5ZL2qiJzL6MBL9cguPg-4-wamSDp2wSHQPE2ZnTWEQ/viewform?embedded=true" title="TahsinProduction" width="100%" height="1450" frameborder="0" marginheight="100%" marginwidth="100%">Loading…</iframe>
 </Layout>)
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="TahsinProduction">
       <SEO title="Home" keywords={[`TahsinProduction`, `Tahsin Prodcution`, `TahsinProduction Home`]} />
-      <img src = "https://s29vla.storage.yandex.net/rdisk/15e6b201fe6914c8f194073329683176f95d2bee0aad89811c0d5c1b7bf13aa6/5dc2ba6e/fKqInKw3d7bLFOeFnMGnhKXFCv-zr-VQXbCeoVHXJrhsxuCtBS8B2nbplSzrvTs8dSUP44fB1dXM3txotWMvfLxRpUhiGHgvxk-agTsA9J-r8npumZHI4midPdWhecNq?uid=1130000040914342&filename=Official%20Cover%20TahsinProduction.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000040914342&fsize=16121709&hid=d63684c22b8ba84e5ae5e5992155e6dd&media_type=image&tknv=v2&etag=e3753f2a7e307d3aabc915a4f3afca57&rtoken=hzz3sEMRXFGR&force_default=yes&ycrid=na-9b92214ca12be776947ff2bc68412c38-downloader10f&ts=596ac9130f780&s=77c15ece090fa1e3f32aecf2a688c733d0251aaf47d110fb02475ff40f8e4e81&pb=U2FsdGVkX1-dBXO6rHKu5HTbFRujvjYbMScply42GBDCT7po6VyfI7atMw5p_3zOqbMXOKuuqkiQqTq1-c1YzlBZpt5OTffwEfqcjAaEKirkPIdHj7swK51MqpH6v2kq" width="100%" height="auto" margin="0 auto" alt="TahsinProduction Tahsin Production" /><br></br><div className="mywhite"><h1>New Articles</h1></div>
+      <img src = "https://i.postimg.cc/rM1htVjr/Official-Cover-Tahsin-Production-1.jpg" width="100%" height="auto" margin="0 auto" alt="TahsinProduction Tahsin Production" /><br></br><div className="mywhite"><h1>New Articles</h1></div>
       <StaticQuery
         query={indexQuery}
         render={data => {
