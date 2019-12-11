321b4ccb927cb1a6399bf62dd40b9d4525c1a986
TahsinProduction <khantahsinkabir@gmail.com>
2019-12-11 12:59:48 +0600

    Revert "Bug Fix"
    This reverts commit 2d9716e470a942e306824d20e44219f8c2c045b2.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import Layout from '../components/layout'
+import SEO from '../components/seo'
 import { graphql, StaticQuery } from 'gatsby'
 import Post from '../components/Post'
 import PaginationLinks from '../components/PaginationLinks'
 
 const IndexPage = () => {
-  const { siteUrl } = useSEO();
   const postsPerPage = 2
   let numberOfPages
   return (
     <Layout pageTitle="TahsinProduction">
+<<<<<<< HEAD
       <SEO title="Home" keywords={[`TahsinProduction`, `Tahsin Prodcution`, `TahsinProduction Home`]} />
+=======
+      <SEO title="TahsinProduction"
+           description="Welcome to TahsinProduction. We are new generation of content production. Start looking at the usual world in a unusual way."
+           pagePath="/"
+           schema={`{
+              "@context": "http://schema.org",
+              "@type": "WebPage",
+              "mainEntity": {
+                "@type": "Organization",
+                "name": "TahsinProduction",
+                "image": "https://images2.imgbox.com/2b/88/NQSvb5zx_o.png"
+              }
+            }`}
+            keywords={[`TahsinProduction`, `Tahsin Prodcution`, `TahsinProduction Home`]} />
+>>>>>>> parent of 2d9716e... Bug Fix
       <img src = "https://images2.imgbox.com/bb/97/iMDQ3WE2_o.jpg" width="100%" height="auto" margin="0 auto" alt="TahsinProduction Tahsin Production" /><br></br><div className="mywhite"><h1>New Articles</h1></div>
       <StaticQuery
         query={indexQuery}
