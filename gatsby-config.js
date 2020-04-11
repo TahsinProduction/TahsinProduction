dc6da25d770205bd987f5e41ccb730b70fb52ead
TahsinProduction <TahsinProduction007@gmail.com>
2020-04-11 13:29:34 +0600

    Revert "Robot.TXT changed"
    This reverts commit 092bd2599e8beeeddabeccb65ccc0ebe8ec03b0f.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,3 @@
-const {
-  NODE_ENV,
-  URL: NETLIFY_SITE_URL = 'https://www.tahsinproduction.com',
-  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
-  CONTEXT: NETLIFY_ENV = NODE_ENV,
-} = process.env
-const isNetlifyProduction = NETLIFY_ENV === 'production'
-const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
 module.exports = {
   siteMetadata: {
     title: `TahsinProduction`,
@@ -14,31 +6,19 @@ module.exports = {
     siteUrl: 'https://www.tahsinproduction.com'
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
     'gatsby-plugin-sass',
     'gatsby-plugin-catch-links',
     'gatsby-transformer-remark',
-    'gatsby-plugin-netlify-cms',
-    'gatsby-plugin-sitemap',
+    `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-robots-txt',
+    `gatsby-plugin-sitemap`,
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        resolveEnv: () => NETLIFY_ENV,
-        env: {
-          production: {
-            policy: [{ userAgent: '*' }],
-          },
-          'branch-deploy': {
-            policy: [{ userAgent: '*', disallow: ['/'] }],
-            sitemap: null,
-            host: null,
-          },
-          'deploy-preview': {
-            policy: [{ userAgent: '*', disallow: ['/'] }],
-            sitemap: null,
-            host: null,
-          },
-        },
+        id: "GTM-5CNMSWD",
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
       },
     },
     {
@@ -58,6 +38,14 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `UA-147244731-1`,
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.tahsinproduction.com',
+        sitemap: 'https://www.tahsinproduction.com/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
       }
     },
     {
