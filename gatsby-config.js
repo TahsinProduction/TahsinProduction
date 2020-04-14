0c074d2e2fc78766ba3506a2b65c73d8d2e55e78
TahsinProduction <TahsinProduction007@gmail.com>
2020-04-14 21:57:46 +0600

    111

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://www.tahsinproduction.com',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 module.exports = {
   siteMetadata: {
     title: `TahsinProduction`,
@@ -43,9 +51,22 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://www.tahsinproduction.com',
-        sitemap: 'https://www.tahsinproduction.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }]
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          }
+        }
       }
     },
     {
